bb8f0652457f2140ad37ccf12ee2c8fe2cd75677
Toon Lee <toonlee.wee.2023@mitb.smu.edu.sg>
2026-02-08 14:13:04 +0800

    Add database setup

---
+++ app/db/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+#Creating the database and connecting SQLite with Python
+DATABASE_URL = "sqlite:///./app.db"
+
+# Create engine
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# Base class for ORM models
+Base = declarative_base()
+
+# Session configuration
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
